5898252a44ed11f7bf3e7ad412a04dc060a6e6f8
abdullah-necib <necib.abdullah@gmail.com>
2025-11-29 15:49:29 +0300

    modify for error

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     print("versions-tracker local run")
     print(f"Remember to login with: gcloud auth application-default login")
 
+
     app = flask.Flask(__name__)  # Create a Flask app instance
     request = flask.request
     main(request)
