855b2881a6066186e6396d09395fdfb6321182af
abdullah-necib <necib.abdullah@gmail.com>
2025-11-29 09:50:28 +0300

    V1

---
+++ main.py
@@ -0,0 +1,17 @@
+import flask
+import functions_framework
+
+
+@functions_framework.http
+def main(request: flask_wrappers.Request) -> flask.Response:
+    log_headline: str = f"main()"
+    print(f"{log_headline} · Init")
+
+
+if __name__ == '__main__':
+    print("versions-tracker local run")
+    print(f"Remember to login with: gcloud auth application-default login")
+
+    app = flask.Flask(__name__)  # Create a Flask app instance
+    request = flask.request
+    main(request)
