10a29c8eaf500fe6ffff2e6782dec5e29779c8a0
abdullah-necib <necib.abdullah@gmail.com>
2025-11-29 16:09:28 +0300

    try fix 1

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import functions_framework
 def main(request: flask.wrappers.Request) -> flask.Response:
     log_headline: str = f"main()"
     print(f"{log_headline} · Init")
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run sets PORT
+    app.run(host="0.0.0.0", port=port) 
 
 
 if __name__ == '__main__':
     print("versions-tracker local run")
     print(f"Remember to login with: gcloud auth application-default login")
+
 
 
     app = flask.Flask(__name__)  # Create a Flask app instance
